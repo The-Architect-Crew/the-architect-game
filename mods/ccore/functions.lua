82dd82b1993977cf37baba5a566ecac3fc17b8f1
lumberjackgames <lumberjackgames@protonmail.com>
2021-12-11 07:33:39 -0500

    fix stuff broken in merge

--- mods/ccore/functions.lua
+++ mods/ccore/functions.lua
@@ -1,13 +1,5 @@
 -- Public functions for shared use by mods
-<<<<<<< HEAD
 function ccore.notify(playername, text, timeout)
     assert(ccore._notify, "ccore._notify not found")
     ccore._notify(playername, text, timeout)
-=======
-function core.notify(playername, text, timeout)
-    -- Wrapper function for notify.lua
-    assert(core._notify, "core._notify not found")
-    core._notify(playername, text, timeout)
->>>>>>> dd5d65cda742a7346149316bbba468e3748c3811
 end
-

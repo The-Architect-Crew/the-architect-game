7245278fd64a3b991343b5ad069489d9fba7340b
lumberjackgames <lumberjackgames@protonmail.com>
2021-12-11 05:27:57 -0500

    remove unused protection mod support

--- mods/tools/replacer.lua
+++ mods/tools/replacer.lua
@@ -230,33 +230,7 @@ replacer.node_is_owned = function(pos, placer)
         end
         return res
     end
-
-    local ownername = false
-    if type(IsPlayerNodeOwner) == "function" then -- node_ownership mod
-        if HasOwner(pos, placer) then -- returns true if the node is owned
-            if not IsPlayerNodeOwner(pos, pname) then
-                if type(getLastOwner) == "function" then -- ...is an old version
-                    ownername = getLastOwner(pos)
-                elseif type(GetNodeOwnerName) == "function" then -- ...is a recent version
-                    ownername = GetNodeOwnerName(pos)
-                else
-                    ownername = "someone"
-                end
-            end
-        end
-
-    elseif type(isprotect) == "function" then -- glomie's protection mod
-        if not isprotect(5, pos, placer) then
-            ownername = "someone"
-        end
-    end
-
-    if ownername ~= false then
-        minetest.chat_send_player(pname, "Sorry, " .. ownername .. " owns that spot.")
-        return true
-    else
-        return false
-    end
+    return false
 end
 
 -- Handle mode setting/getting

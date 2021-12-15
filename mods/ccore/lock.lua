11ae25d82bcb2993922d5a3aea0de04d716aa0ad
neinwhal <fishyWET@hotmail.com>
2021-12-16 05:02:27 +0800

    Add crates
    - Move notify code all into notify.lua
    - Add a bunch of new icons
    - Add base of crates
    - Add locks API
    - Change signs to use locks API

---
+++ mods/ccore/lock.lua
@@ -0,0 +1,99 @@
+locks = {}
+locks.dinfo = {}
+locks.dinfo["lock"] = {"Only the owner & shared can access & dig", "Locked"}
+locks.dinfo["unlock"] = {"Everyone can access & dig", "Public Access"}
+locks.dinfo["mail"] = {"Everyone can add items, but only owner & shared can access & dig", "Mailbox"}
+locks.dinfo["protect"] = {"If protected: The owner & shared & protection owner(s) can access & dig \nIf not protected: Everyone can access & dig", "Protected Access"}
+
+-- returns description
+function locks.desc(type, level, disable_shared)
+	local desc = locks.dinfo[type][level]
+	if disable_shared then
+		local desc2 = desc:gsub(" & shared", "")
+		return desc2
+	else
+		return desc
+	end
+end
+
+-- returns current order, next order
+function locks.icons(fieldname, lock, order, disable_shared, add_desc)
+	local adesc = ""
+	if not add_desc then
+		adesc = ""
+	else
+		adesc = " \n"..add_desc
+	end
+	for index, row in pairs(order) do
+		if row == lock then
+			local indexplus = index + 1
+			if indexplus > #order then
+				indexplus = 1
+			else
+				indexplus = index + 1
+			end
+			local cicon, nicon = order[index], order[indexplus]
+			return "gui_"..cicon..".png;"..fieldname.."_"..nicon..";;true;false;]"..
+				"tooltip["..fieldname.."_"..nicon..";Current status: "..cicon.." \n"..locks.desc(cicon, 1, disable_shared).." \nPress to change to: "..nicon..""..adesc.."]"
+		end
+	end
+end
+
+-- check whether valid access
+-- returns true if able to access protected area + has edit rights
+-- return <value> if has edit rights but not access protected area 
+-- return "mail" if has mail rights
+function locks.can_access(pos, player)
+	local meta = minetest.get_meta(pos)
+	local playername = player:get_player_name()
+	local owner = meta:get_string("owner")
+	local lock = meta:get_string("lock")
+	local shared = meta:get_string("shared")
+	-- protection bypass
+	if minetest.check_player_privs(player, "protection_bypass") then
+		return true
+	end
+	-- no owner
+	if owner == "" then
+		if minetest.is_protected(pos, playername) then
+			return "noone"
+		else
+			return true
+		end
+	end
+	-- owner
+	if playername == owner then
+		if minetest.is_protected(pos, playername) then
+			return "owner"
+		else
+			return true
+		end
+	end
+	-- shared
+	local sharedstring = " "..shared.." "
+	if sharedstring:find(" "..playername.." ") then
+		if minetest.is_protected(pos, playername) then
+			return "shared"
+		else
+			return true
+		end
+	end
+	-- public
+	if lock == "unlock" then
+		if minetest.is_protected(pos, playername) then
+			return "public"
+		else
+			return true
+		end
+	end
+	-- protect
+	if lock == "protect" then
+		if not minetest.is_protected(pos, playername) then
+			return "protect"
+		end
+	end
+	-- mail
+	if lock == "mail" then
+		return "mail"
+	end
+end
---
+++ mods/crates/init.lua
@@ -0,0 +1,322 @@
+crates = {}
+crates.pos = {}
+
+-- Inventory sorting ( Taken from https://github.com/minetest-mods/technic/blob/master/technic_chests/register.lua )
+local function sort_inventory(inv, sorttype, tslots)
+	local inlist = inv:get_list("main")
+	local typecnt = {}
+	local typekeys = {}
+	for _, st in ipairs(inlist) do
+		if not st:is_empty() then
+			local n = st:get_name()
+			local w = st:get_wear()
+			local m = st:get_metadata()
+			local k = string.format("%s %05d %s", n, w, m)
+			if not typecnt[k] then
+				typecnt[k] = {st}
+				table.insert(typekeys, k)
+			else
+				table.insert(typecnt[k], st)
+			end
+		end
+	end
+	table.sort(typekeys)
+	inv:set_list("main", {})
+	for _, k in ipairs(typekeys) do
+		for _, item in ipairs(typecnt[k]) do
+			inv:add_item("main", item)
+		end
+	end
+end
+
+-- storage API
+function crates:register_storage(name, def)
+	local desc = def.description
+	local scolumns = tonumber(def.columns) or 1
+	local colw = scolumns * 1.25
+	local locko = def.lock_order
+	-- storage formspec
+	local function storage_formspec(pos)
+		local spos = pos.x..","..pos.y..","..pos.z
+		local meta = minetest.get_meta(pos)
+		local label = meta:get_string("label")
+		local shared = meta:get_string("shared")
+		local lock = meta:get_string("lock")
+		if def.sorting and scolumns > 1 then
+			crates.sorting_formspec =
+				"button[10.65,1.8;0.5,0.5;storage_sort;S]"..
+				"tooltip[storage_sort;Sort everything]"..
+				"button[10.65,2.4;0.5,0.5;storage_row_up;Ʌ]"..
+				"tooltip[storage_row_up;Move everything one row up]"..
+				"button[10.65,3.0;0.5,0.5;storage_row_down;V]"..
+				"tooltip[storage_row_down;Move everything one row down]"
+		elseif def.sorting and scolumns == 1 then
+			crates.sorting_formspec =
+				"button[10.65,1.8;0.5,0.5;storage_sort;S]"..
+				"tooltip[storage_sort;Sort everything]"
+		else
+			crates.sorting_formspec = ""
+		end
+		
+		return "formspec_version[4]"..
+			"size[10.55,"..(8.35 + colw).."]"..
+			-- bg
+			"style_type[image;noclip=true]"..
+			"style_type[button;noclip=true;bgimg=gui_bg_curved.png;bgimg_hovered=gui_bg_hover_curved.png;border=false]"..
+			"image[-1.4,"..(6.9 + colw)..";1.4,1.4;gui_tab.png]"..
+			--"image[10.54,"..(6.9 + colw)..";1.4,1.4;gui_tab.png^[transformFX]"..
+			--"image[10.54,0.3;1.4,1.4;gui_tab.png^[transformFX]"..
+			"box[0.2,1.6;10.15,"..(0.15 + colw)..";#707070]"..
+			-- label
+			"field[0.375,0.6;9.2,0.8;storage_label;"..desc.." Label;"..label.."]"..
+			"field_close_on_enter[storage_label;false]"..
+			"image_button[9.5,0.47;1,1;gui_enter.png;storage_label_save;;true;false;gui_enter.png]"..
+			-- storage
+			"list[nodemeta:"..spos..";main;0.4,1.8;8,"..scolumns..";]"..
+			-- player
+			"list[current_player;main;0.4,"..(1.95 + colw)..";8,4;]"..
+			-- locks
+			"image_button[-1.1,"..(7.05 + colw)..";1.05,1.05;"..locks.icons("storage", lock, locko)..
+			"field[0.375,"..(7.2 + colw)..";9.2,0.8;storage_shared;Shared with (separate names with spaces):;"..shared.."]"..
+			"field_close_on_enter[storage_shared;false]"..
+			"tooltip[storage_shared;Player names here will have access regardless of locked status \nOnly the owner can change settings \nFormat: 'name1 name2 name3...']"..
+			"image_button[9.5,"..(7.07 + colw)..";1,1;gui_enter.png;storage_shared_save;;true;false;gui_enter.png]"..
+			-- sorting
+			crates.sorting_formspec..
+			-- listring
+			"listring[nodemeta:" .. spos .. ";main]"..
+			"listring[current_player;main]"
+	end
+	-- label display
+	local function label_display(label)
+		if label == "" then
+			return ""
+		else
+			return "Label: "..label
+		end
+	end
+	-- show formspec
+	local function show_formspec(playername, pos)
+		minetest.show_formspec(playername, name, storage_formspec(pos))
+	end
+	-- storage block
+	minetest.register_node(name, {
+		description = desc,
+		drawtype = def.drawtype,
+		mesh = def.mesh,
+		visual_scale = def.visual_scale,
+		node_box = def.node_box,
+		selection_box = def.selection_box,
+		collision_box = def.collision_box,
+		tiles = def.tiles,
+		overlay_tiles = def.overlay_tiles,
+		special_tiles = def.special_tiles,
+		inventory_image = def.inventory_image,
+		inventory_overlay = def.inventory_overlay,
+		wield_image = def.wield_image,
+		wield_overlay = def.wield_overlay,
+		wield_scale = def.wield_scale,
+		paramtype = def.paramtype,
+		paramtype2 = def.paramtype2,
+		sunlight_propagates = def.sunlight_propagates,
+		use_texture_alpha = def.use_texture_alpha,
+		range = def.range,
+		walkable = def.walkable,
+		pointable = def.pointable,
+		diggable = def.diggable,
+		climbable = def.climbable,
+		move_resistance = def.move_resistance,
+		buildable_to = def.buildable_to,
+		floodable = def.floodable,
+		leveled = def.leveled,
+		leveled_max = def.leveled_max,
+		light_source = def.light_source,
+		waving = def.waving,
+		groups = def.groups,
+		drop = def.drop,
+		sounds = def.sounds,
+		mod_origin = def.mod_origin,
+		on_construct = function(pos)
+			local meta = minetest.get_meta(pos)
+			local inv = meta:get_inventory()
+			inv:set_size("main", 8 * scolumns)
+			meta:set_string("owner", "")
+			meta:set_string("label", "")
+			meta:set_string("shared", "")
+			meta:set_string("lock", locko[1])
+			meta:set_string("infotext", locks.desc(locko[1], 2).." "..desc.." \nUnowned")
+		end,
+		after_place_node = function(pos, placer, itemstack, pointed_thing)
+			local meta = minetest.get_meta(pos)
+			local playername = placer:get_player_name()
+			meta:set_string("owner", playername)
+			meta:set_string("infotext", locks.desc(locko[1], 2).." "..desc.." (Owned by "..playername..")")
+		end,
+		on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+			local meta = minetest.get_meta(pos)
+			local playername = clicker:get_player_name()
+			if locks.can_access(pos, clicker) then
+				show_formspec(playername, pos)
+				crates.pos[playername] = pos
+			end
+		end,
+		allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+			local ints = locks.can_access(pos, player)
+			if ints then
+				if ints == "mail" then
+					return 0
+				else
+					return count
+				end
+			end
+			return 0
+		end,
+		allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+			if locks.can_access(pos, player) then
+				return stack:get_count()
+			end
+			return 0
+		end,
+		allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+			local ints = locks.can_access(pos, player)
+			if ints then
+				if ints == "mail" then
+					return 0
+				else
+					return stack:get_count()
+				end
+			end
+			return 0
+		end,
+		on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+			minetest.log("action", player:get_player_name() ..
+				" moves stuff in "..name.." at " .. minetest.pos_to_string(pos))
+		end,
+		on_metadata_inventory_put = function(pos, listname, index, stack, player)
+			minetest.log("action", player:get_player_name() ..
+				" moves " .. stack:get_name() ..
+				" to "..name.." at " .. minetest.pos_to_string(pos))
+		end,
+		on_metadata_inventory_take = function(pos, listname, index, stack, player)
+			minetest.log("action", player:get_player_name() ..
+				" takes " .. stack:get_name() ..
+				" from "..name.." at " .. minetest.pos_to_string(pos))
+		end,
+		can_dig = function(pos, player)
+			local meta = minetest.get_meta(pos);
+			local inv = meta:get_inventory()
+			if locks.can_access(pos, player) == true and inv:is_empty("main") then
+				return true
+			end
+		end,
+		on_blast = function() end,
+	})
+	
+	minetest.register_on_player_receive_fields(function(player, formname, fields)
+		if formname ~= name or not player then
+			return
+		end
+		local playername = player:get_player_name()
+		local pos = crates.pos[playername]
+		local meta = minetest.get_meta(pos)
+		local owner = meta:get_string("owner")
+		local lock = meta:get_string("lock")
+		local label = meta:get_string("label")
+		if playername == owner then
+			-- locks
+			if fields.storage_lock then
+				meta:set_string("lock", "lock")
+				show_formspec(playername, pos)
+				meta:set_string("infotext", locks.desc("lock" , 2).." "..desc.." (Owned by "..owner..") \n"..label_display(label))
+			elseif fields.storage_protect then
+				meta:set_string("lock", "protect")
+				show_formspec(playername, pos)
+				meta:set_string("infotext", locks.desc("protect" , 2).." "..desc.." (Owned by "..owner..") \n"..label_display(label))
+			elseif fields.storage_mail then
+				meta:set_string("lock", "mail")
+				show_formspec(playername, pos)
+				meta:set_string("infotext", locks.desc("mail" , 2).." "..desc.." (Owned by "..owner..") \n"..label_display(label))
+			elseif fields.storage_unlock then
+				meta:set_string("lock", "unlock")
+				show_formspec(playername, pos)
+				meta:set_string("infotext", locks.desc("unlock" , 2).." "..desc.." (Owned by "..owner..") \n"..label_display(label))
+			end
+			-- labeling
+			if fields.storage_label_save or fields.key_enter_field == "storage_label" then
+				local newlabel = fields.storage_label
+				meta:set_string("label", newlabel)
+				meta:set_string("infotext", locks.desc(lock , 2).." "..desc.." (Owned by "..owner..") \n"..label_display(newlabel))
+				show_formspec(playername, pos)
+			end
+			-- sharing
+			if fields.storage_shared_save or fields.key_enter_field == "storage_shared" then
+				local newshared = fields.storage_shared
+				meta:set_string("shared", newshared)
+				show_formspec(playername, pos)
+			end
+		end
+		
+		-- sorting
+		-- allow shared and owner access always
+		-- if protected + "protect/unlock": allow protection owner(s)
+		-- if unprotected + "protect/unlock": allow everyone
+		local ints = locks.can_access(pos, player)
+		if ints and ints ~= "mail" and ints ~= "public" then
+			local inv = meta:get_inventory()
+			local tslots = scolumns * 8
+			local uslots = tslots - 8
+			if fields.storage_row_up then
+				local inlist = inv:get_list("main")
+				local newlist = {}
+				for i = 1, 8 do
+					newlist[uslots+i] = inlist[i]
+				end
+				for i = 8, tslots do
+					if (i - 8) == 0 then
+						newlist[1] = inlist[i]
+					else
+						newlist[i-8] = inlist[i]
+					end
+				end
+				inv:set_list("main", newlist)
+			elseif fields.storage_row_down then
+				local inlist = inv:get_list("main")
+				local newlist = {}
+				for i = uslots, tslots do
+					if (uslots - i) == 0 then
+						newlist[1] = inlist[i]
+					else
+						newlist[i-uslots] = inlist[i]
+					end
+				end
+				for i = 1, uslots do
+					newlist[i+8] = inlist[i]
+				end
+				inv:set_list("main", newlist)
+			elseif fields.storage_sort then
+				sort_inventory(inv, "all")
+			end
+		end
+		
+		if fields.quit then
+			crates.pos[playername] = nil
+		end
+	end)
+end
+
+crates:register_storage("crates:crate", {
+	description = "Crate",
+	columns = 4,
+	sorting = true,
+	portable = true,
+	lock_order = {"lock", "protect", "unlock", "mail"},
+	drawtype = "mesh",
+	mesh = "crate.obj",
+	groups = {oddly_breakable_by_hand = 1},
+	tiles = {"crate.png"},
+	sunlight_propagates = true,
+	use_texture_alpha = "opaque",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sounds = default.node_sound_wood_defaults(),
+})

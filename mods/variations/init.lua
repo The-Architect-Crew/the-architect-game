df35fc6b58f236ec9008ef426ccc7e653e054cdd
neinwhal <3191240+neinwhal@users.noreply.github.com>
2022-08-10 23:24:09 +0800

    Fix accidental revertion of some variations

--- mods/variations/init.lua
+++ mods/variations/init.lua
@@ -238,8 +238,10 @@ variations.register_for_base("blocks:mud")
 variations.register_for_base("blocks:porphyry")
 variations.register_for_base("blocks:serpentine")
 variations.register_for_base("blocks:slate")
+variations.register_for_base("blocks:mineral_salt")
 variations.register_for_base("blocks:concrete")
-
+variations.register_for_base("blocks:wood")
+variations.register_for_base("blocks:mese")
 
 variations.register_support("blocks:stone")
 variations.register_support("blocks:sandstone")

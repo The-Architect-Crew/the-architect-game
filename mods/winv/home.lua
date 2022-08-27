43a2a95208d809081f8a879e2e17c5700e05c4b2
neinwhal <3191240+neinwhal@users.noreply.github.com>
2022-08-27 23:20:35 +0800

    Improve waypoints handling

--- mods/winv/home.lua
+++ mods/winv/home.lua
@@ -61,15 +61,23 @@ local function get_homes_amt(player, type)
 	local meta = player:get_meta()
 	local homespd = meta:get_string("whomes")
 	if not homespd or homespd == "" then
-		return 0
+		return 0, 0
 	end
 	local homesdata = minetest.deserialize(homespd)
 	local homes_amt = 0
+	local total_amt = 0
 	local waypoint_amt = 0
-	for i, x in pairs(homesdata) do
-		homes_amt = homes_amt + 1
+	for i, homelist in pairs(homesdata) do
+		if type and homelist.type == type then
+			homes_amt = homes_amt + 1
+		end
+		total_amt = total_amt + 1
 	end
-	return homes_amt
+	if type then
+		return homes_amt, total_amt
+	else
+		return total_amt
+	end
 end
 
 local function create_ordered_table(homesdata)
@@ -86,22 +94,22 @@ local function add_home(player, hname, htype)
 	local hinv = whomes_inventory[name] or init_homes(player)
 	if not hname or hname == "" then
 		homes_chat(name, "Please add a name for the "..htype.."!")
-		return
+		return false
 	end
 	local meta = player:get_meta()
-	local homes_amt = get_homes_amt(player)
+	local homes_amt, total_amt = get_homes_amt(player, "home")
 	local homes_limit = get_homes_limit(player)
 	local homesdata = minetest.deserialize(meta:get_string("whomes")) or {}
 	if htype == "home" and homesdata and (homes_amt + 1) > homes_limit then -- check home limit
 		homes_chat(name, "You have reached your limit of "..homes_limit.." "..htype.."s! Delete existing ones.")
-		return
+		return false
 	end
 	if homesdata[hname] then -- ensure name isnt already registered
-		homes_chat(name, hname.." is already a registered "..htype.."! Please pick another name or delete the existing entry.")
-		return
+		homes_chat(name, hname.." is already registered! Please pick another name or delete the existing entry.")
+		return false
 	end
 	local ppos = player:get_pos()
-	local order = homes_amt + 1
+	local order = total_amt + 1
 	if htype == "waypoint" then
 		local color = math.random(0xFFFFFF)
 		homesdata[hname] = {x = ppos.x, y = ppos.y, z = ppos.z, type = "waypoint", icon = math.random(1, #whomes_icons), hud = true, color = color, order = order}
@@ -138,9 +146,15 @@ local function delete_home(player, hname)
 				end
 			end
 		end
+		local hinv = whomes_inventory[name] or init_homes(player)
+		local wp_id = hinv.waypoints[hname]
+		if wp_id then -- remove waypoint if it exists
+			player:hud_remove(wp_id)
+		end
+		local htype = homesdata[hname].type
 		homesdata[hname] = nil
 		meta:set_string("whomes", minetest.serialize(homesdata))
-		homes_chat(name, "Removed '"..hname.."' from your list of homes!")
+		homes_chat(name, "Removed '"..hname.."' from your list of "..htype.."s!")
 	else
 		homes_chat(name, "Invalid home selected. Please refresh.")
 	end
@@ -155,11 +169,16 @@ local function tp_home(player, hname)
 	local meta = player:get_meta()
 	local homesdata = minetest.deserialize(meta:get_string("whomes")) or {}
 	if homesdata[hname] then
-		local hpos = homesdata[hname]
-		player:set_pos(hpos)
-		homes_chat(name, "Travelling to '"..hname.."' home ("..hpos.x..", "..hpos.y..", "..hpos.z..")...")
+		local htype = homesdata[hname].type
+		if htype == "home" then
+			local hpos = homesdata[hname]
+			player:set_pos(hpos)
+			homes_chat(name, "Travelling to '"..hname.."' home ("..hpos.x..", "..hpos.y..", "..hpos.z..")...")
+		elseif htype ~= "home" then
+			homes_chat(name, hname.." is a "..htype..", you can only travel to homes!")
+		end
 	else
-		homes_chat(name, "Invalid home selected. Please refresh.")
+		homes_chat(name, "Invalid home selected.")
 	end
 end
 
@@ -170,7 +189,7 @@ local function change_order(player, chhome, direction)
 	local homelist = homesdata[chhome]
 	local otable = create_ordered_table(homesdata)
 	local horder = homelist.order
-	local homes_amt = get_homes_amt(player)
+	local total_amt = get_homes_amt(player)
 	if otable[horder] == chhome then
 		if direction == "up" then
 			if (horder - 1) < 1 then
@@ -184,7 +203,7 @@ local function change_order(player, chhome, direction)
 				winv.refresh(player)
 			end
 		elseif direction == "down" then
-			if (horder + 1) > homes_amt then
+			if (horder + 1) > total_amt then
 				homes_chat(name, chhome.." is already at the bottom!")
 				return
 			else
@@ -209,6 +228,9 @@ local function hud_waypoint(player, hname)
 	local hinv = whomes_inventory[name] or init_homes(player)
 	local wp_id = hinv.waypoints[hname]
 	local homelist = homesdata[hname]
+	if not homelist then
+		return
+	end
 	if wp_id then -- remove waypoint if it exists
 		player:hud_remove(wp_id)
 		hinv.waypoints[hname] = nil
@@ -254,9 +276,12 @@ winv:register_inventory("home", {
 			for i, hname in ipairs(otable) do
 				local homelist = homesdata[hname]
 				local yshift = 2 * (i - 1)
-				local icon_bg = "gui_bg_curved.png"
-				local icon_bg_pressed = "gui_bg_hover_curved.png"
-				local colorize_arrows = ""
+				local icon_bg, icon_bg_pressed = "gui_bg_curved.png", "gui_bg_hover_curved.png"
+				local colorize_arrows, activate_wp = "", ""
+				local hname_label = hname
+				if #hname > 15 then
+					hname_label = string.sub(hname, 1, 13).."..."
+				end
 				if homelist.color then
 					local hexcolor = string.format("%02x", homelist.color)
 					while #hexcolor < 6 do
@@ -266,7 +291,11 @@ winv:register_inventory("home", {
 					icon_bg_pressed = "gui_bg_hover_curved.png^[colorize:#"..hexcolor..":90"
 					colorize_arrows = "^[colorize:#"..hexcolor..":100"
 				end
+				if homelist.hud then
+					activate_wp = "^[brighten"
+				end
 				local waypoint_buttons = ""
+				local coords = "("..ccore.round(homelist.x, 1)..", "..ccore.round(homelist.y, 1)..", "..ccore.round(homelist.z, 1)..")"
 				if homelist.type == "waypoint" then
 					waypoint_buttons =
 						"image_button[6.175,"..(0.375 + yshift)..";0.7,0.7;gui_cross.png;winv_homes_del_"..hname..";]"
@@ -279,12 +308,13 @@ winv:register_inventory("home", {
 					"box[0.25,"..(0.25 + yshift)..";6.75,1.75;#94949470]"..
 					"image_button[0.375,"..(0.375 + yshift)..";1.5,1.5;"..icon_bg..";winv_homes_icon_"..hname..";;;false;"..icon_bg_pressed.."]"..
 					"item_image[0.545,"..(0.54 + yshift)..";1.2,1.2;"..whomes_icons[homelist.icon].."]"..
-					"label[2.1,"..(1.0 + yshift)..";"..hname.."\n("..ccore.round(homelist.x, 1)..", "..ccore.round(homelist.y, 1)..", "..ccore.round(homelist.z, 1)..")]"..
+					"label[2.1,"..(1.0 + yshift)..";"..hname_label.."\n"..coords.."]"..
 					"image_button[4.575,"..(0.375 + yshift)..";0.7,0.7;gui_paintbrush.png;winv_homes_color_"..hname..";]"..
-					"image_button[5.375,"..(0.375 + yshift)..";0.7,0.7;gui_location.png;winv_homes_wp_"..hname..";]"..
+					"image_button[5.375,"..(0.375 + yshift)..";0.7,0.7;gui_location.png"..activate_wp..";winv_homes_wp_"..hname..";]"..
 					waypoint_buttons..
 					"image_button[0.35,"..(0.4 + yshift)..";0.5,0.5;gui_pointer.png^[transformR90"..colorize_arrows..";winv_homes_up_"..hname..";;;false;]"..
-					"image_button[0.35,"..(1.35 + yshift)..";0.5,0.5;gui_pointer.png^[transformR90FY"..colorize_arrows..";winv_homes_down_"..hname..";;;false;]"
+					"image_button[0.35,"..(1.35 + yshift)..";0.5,0.5;gui_pointer.png^[transformR90FY"..colorize_arrows..";winv_homes_down_"..hname..";;;false;]"..
+					"tooltip[2,"..(0.45 + yshift)..";2.5,1.3;"..hname.."\n"..coords.."]"
 			end
 		end
 		local homes_limit = get_homes_limit(player)
@@ -316,8 +346,8 @@ winv:register_inventory("home", {
 			"field[0.25,6.8;4.65,0.7;winv_home_name;;]",
 			"image_button[5.15,6.8;1.05,0.7;winv_add_home.png;winv_home_add;;;true;winv_add_home.png]",
 			"image_button[6.45,6.8;1.05,0.7;winv_add_waypoint.png;winv_home_add_wp;;;true;winv_add_waypoint.png]",
-			"tooltip[winv_home_add;Add a home \nYour homes limit: "..homes_limit.." homes\nPosition based on your standing location \nFill field on the left with home naming]",
-			"tooltip[winv_home_add_wp;Add a waypoint \nYour homes limit: "..homes_limit.." homes\nPosition based on your standing location \nFill field on the left with home naming]",
+			"tooltip[winv_home_add;Add a home \nYour homes limit: "..homes_limit.." homes\nPosition based on your current location \nFill field on the left with home name]",
+			"tooltip[winv_home_add_wp;Add a waypoint \nPosition based on your current location \nFill field on the left with waypoint name]",
 		}
 		return table.concat(formspec, "")
 	end,
@@ -327,12 +357,16 @@ winv:register_inventory("home", {
 		local name = player:get_player_name()
 		local hinv = whomes_inventory[name] or init_homes(player)
 		if fields.winv_home_add then
-			add_home(player, fields.winv_home_name)
-			winv.refresh(player)
+			local hname = minetest.formspec_escape(fields.winv_home_name)
+			if add_home(player, hname) ~= false then
+				winv.refresh(player)
+			end
 		elseif fields.winv_home_add_wp then
-			add_home(player, fields.winv_home_name, "waypoint")
-			hud_waypoint(player, fields.winv_home_name)
-			winv.refresh(player)
+			local hname = minetest.formspec_escape(fields.winv_home_name)
+			if add_home(player, hname, "waypoint") ~= false then
+				hud_waypoint(player, fields.winv_home_name)
+				winv.refresh(player)
+			end
 		elseif fields.winv_home_scroll then
 			local scroll = minetest.explode_scrollbar_event(fields.winv_home_scroll)
 			hinv.scroll = scroll.value
@@ -350,6 +384,7 @@ winv:register_inventory("home", {
 					end
 				elseif fields["winv_homes_wp_"..hname] then
 					hud_waypoint(player, hname)
+					winv.refresh(player)
 				elseif fields["winv_homes_icon_"..hname] then
 					local newicon = homesdata[hname].icon + 1
 					if newicon > #whomes_icons then
@@ -419,6 +454,7 @@ minetest.register_chatcommand("sethome", {
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 		add_home(player, param)
+		winv.refresh(player)
 	end,
 })
 
@@ -428,6 +464,7 @@ minetest.register_chatcommand("delhome", {
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 		delete_home(player, param)
+		winv.refresh(player)
 	end,
 })
 
@@ -436,7 +473,20 @@ minetest.register_chatcommand("home", {
 	description = "Travel to your homes.",
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
-		local meta = player:get_meta()
-		local homesdata = minetest.deserialize(meta:get_string("whomes"))
+		tp_home(player, param)
 	end,
 })
+
+-- resume waypoint huds
+minetest.register_on_joinplayer(function(player)
+	local meta = player:get_meta()
+	local homesdata = minetest.deserialize(meta:get_string("whomes"))
+	if not homesdata then
+		return
+	end
+	for hname, homelist in pairs(homesdata) do
+		if homelist.hud then
+			hud_waypoint(player, hname)
+		end
+	end
+end)

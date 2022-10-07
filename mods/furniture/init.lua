0ad3019256e4d056a7dc1c3cac6b70885b032a85
Corvus262 <gavranvukhome@gmail.com>
2022-10-07 18:07:48 +0200

    Add active furniture support (doors), refactor code

--- mods/furniture/init.lua
+++ mods/furniture/init.lua
@@ -103,6 +103,17 @@ furniture.types = {
         storage = 4,
         box = {-1.0, -0.5, -0.0625, 1.0, 0.5, 0.5}
     },
+    {
+        name = "door",
+        description = "Door",
+        type = "active",
+        activate_sound = {default = "doors_door_open", wood = "doors_door_open", steelblock = "doors_steel_door_open"},
+        deactivate_sound = {default = "doors_door_close", wood = "doors_door_close", steelblock = "doors_steel_door_close"},
+        special_material = false,
+        generate_locked = true,
+        box = {-0.5, -0.5, -0.5, 0.5, 1.5, -6/16},
+        box_activated = {-0.5, -0.5, -0.5, -6/16, 1.5, 0.5}
+    },
     -- Stonelike
     {
         name = "window_glass_thick",
@@ -239,6 +250,7 @@ furniture.woodlike_set = {
     "wardrobe",
     "mirror",
     "flowerpot",
+    "door",
 }
 
 furniture.stonelike_set = {
@@ -276,85 +288,219 @@ furniture.table_contains = function(term, table)
     end
 end
 
+-- Don't remember if we did this already somewhere else
+furniture.table_copy = function(table)
+    local copy = {}
+    for table_key, table_value in pairs(table) do
+        copy[table_key] = table_value
+    end
+    return copy
+end
+
 function furniture.register(base_node, limit)
     for i=1, #furniture.types do
-        local base_definition = minetest.registered_nodes[base_node]
-        if (furniture.table_contains(furniture.types[i].name, limit)) then
-            local sname = string.match(base_node, ':(.*)')
-            local furniture_name = "furniture:" .. furniture.types[i].name .. "_" .. sname
-            local furniture_description =  base_definition.description .. " " .. furniture.types[i].description
-            local furniture_mesh = (furniture.types[i].base or furniture.types[i].name) .. ".obj"
-            local tiles = {"variations_" .. sname .. ".png"}
-            local alpha = base_definition.use_texture_alpha
-            if (furniture.types[i].special_material) then           
-                local special_definition
-                if (type(furniture.types[i].special_material) == "table") then -- In case of multiple materials
-                    local special_definition
-                    local special_tile
-                    for j=1, #furniture.types[i].special_material do
-                        special_definition = minetest.registered_nodes[furniture.types[i].special_material[j]]
-                        special_tile = special_definition.tiles[1]
-                        tiles[j + 1] = special_tile -- Since the first tile is base
-                    end
-                    alpha = "blend" -- Just in case that transparency is needed
+        local fdef = furniture.types[i]
+        if (furniture.table_contains(fdef.name, limit)) then
+            if (fdef.type == "active") then
+            furniture.generate_active(base_node, i)
+            elseif (fdef.type == "storage") then
+                furniture.generate_storage(base_node, i)
+            else
+                furniture.generate_normal(base_node, i)
+            end
+        end
+    end
+end
+
+function furniture.pick_special_textures(base_node, fdefid, tiles, furniture_name)
+    local fdef = furniture.types[fdefid]
+    local special_definition
+    local special_sname
+    if (type(fdef.special_material) == "table") then -- In case of multiple materials
+        local special_tile
+        special_sname = string.match(fdef.special_material[1], ':(.*)') -- Take first node as identifier (for now)
+        for j=1, #fdef.special_material do
+            special_definition = minetest.registered_nodes[fdef.special_material[j]]
+            special_tile = special_definition.tiles[1]
+            tiles[j + 1] = special_tile -- Since the first tile is base
+        end
+        return tiles, furniture_name .. "_" .. special_sname
+    else
+        special_node = fdef.special_material
+        special_definition = minetest.registered_nodes[special_node]
+        special_sname = string.match(special_node, ':(.*)')
+        tiles[2] = special_definition.tiles[1]
+        alpha = special_definition.use_texture_alpha
+        return  tiles, furniture_name .. "_" .. special_sname
+    end
+end
+
+function furniture.generate_normal(base_node, fdefid)
+    local base_definition = minetest.registered_nodes[base_node]
+    local fdef = furniture.types[fdefid]
+    local sname = string.match(base_node, ':(.*)')
+    local furniture_name = "furniture:" .. fdef.name .. "_" .. sname
+    local furniture_description =  base_definition.description .. " " .. fdef.description
+    local furniture_mesh = (fdef.base or fdef.name) .. ".obj"
+    local tiles = {"variations_" .. sname .. ".png^[sheet:3x3:1,0"}
+    local alpha = base_definition.use_texture_alpha
+    local sounds = base_definition.sounds
+    if (fdef.special_material) then          
+        tiles, furniture_name = furniture.pick_special_textures(base_node, fdefid, tiles, furniture_name)
+        alpha = "blend" -- Just in case that transparency is needed
+    end
+
+    local collision_box = {
+        type = "fixed",
+        fixed = fdef.box,
+    }
+
+    minetest.register_node(furniture_name, {
+        description = furniture_description,
+        tiles = tiles,
+        groups = base_definition.groups,
+        drawtype = 'mesh',
+        mesh = furniture_mesh,
+        collision_box = collision_box,
+        selection_box = collision_box,
+        use_texture_alpha = alpha,
+        paramtype = "light",
+        paramtype2 = "facedir",
+        sunlight_propagates = true,
+        sounds = sounds,
+    })
+end
+
+function furniture.generate_storage(base_node, fdefid)
+end
+
+function furniture.generate_active(base_node, fdefid)
+    local fdef = furniture.types[fdefid]
+    local base_definition = minetest.registered_nodes[base_node]
+    local sname = string.match(base_node, ':(.*)')
+    local furniture_name = "furniture:" .. fdef.name .. "_" .. sname
+    local furniture_description =  base_definition.description .. " " .. fdef.description
+    local furniture_mesh = (fdef.base or fdef.name) .. ".obj"
+    local tiles = {"variations_" .. sname .. ".png^[sheet:3x3:1,0"}
+    local alpha = base_definition.use_texture_alpha
+    local sounds = base_definition.sounds
+    local groups_hidden = furniture.table_copy(base_definition.groups)
+    groups_hidden.not_in_creative_inventory = 1
+
+    if (fdef.special_material) then           
+        tiles, furniture_name = furniture.pick_special_textures(base_node, fdefid, tiles, furniture_name)
+        alpha = "blend" -- Just in case that transparency is needed
+    end
+
+    local collision_box = {
+        type = "fixed",
+        fixed = fdef.box,
+    }
+
+    local collision_box_activated = {
+        type = "fixed",
+        fixed = fdef.box_activated,
+    }
+
+    minetest.register_node(furniture_name, {
+        description = furniture_description,
+        tiles = tiles,
+        groups = base_definition.groups,
+        drawtype = 'mesh',
+        mesh = furniture_mesh,
+        collision_box = collision_box,
+        selection_box = collision_box,
+        use_texture_alpha = alpha,
+        paramtype = "light",
+        paramtype2 = "facedir",
+        sunlight_propagates = true,
+        sounds = sounds,
+        on_rightclick = function(pos, node)
+            minetest.swap_node(pos, {name = furniture_name .. "_activated", param2 = node.param2})
+            minetest.sound_play(fdef.activate_sound[sname] or fdef.activate_sound.default,
+            {pos = pos, max_hear_distance = 10}, true)
+        end
+    })
+    minetest.register_node(furniture_name .. "_activated", {
+        description = furniture_description,
+        tiles = tiles,
+        groups = groups_hidden,
+        drawtype = 'mesh',
+        mesh = (fdef.base or fdef.name) .. "_activated.obj",
+        collision_box = collision_box_activated,
+        selection_box = collision_box_activated,
+        use_texture_alpha = alpha,
+        paramtype = "light",
+        paramtype2 = "facedir",
+        sunlight_propagates = true,
+        sounds = sounds,
+        drop = furniture_name,
+        on_rightclick = function(pos, node)
+            minetest.swap_node(pos, {name = furniture_name, param2 = node.param2})
+            minetest.sound_play(fdef.deactivate_sound[sname] or fdef.deactivate_sound.default,
+            {pos = pos, max_hear_distance = 10}, true)
+        end
+    })
+    if fdef.generate_locked == true then
+        minetest.register_node(furniture_name .. "_locked", {
+            description = "Locked " .. furniture_description,
+            tiles = tiles,
+            groups = base_definition.groups,
+            drawtype = 'mesh',
+            mesh = furniture_mesh,
+            collision_box = collision_box,
+            selection_box = collision_box,
+            use_texture_alpha = alpha,
+            paramtype = "light",
+            paramtype2 = "facedir",
+            sunlight_propagates = true,
+            sounds = sounds,
+            after_place_node = function(pos, placer)
+                local meta = minetest.get_meta(pos)
+                local playername = placer:get_player_name()
+                meta:set_string("owner", playername)
+                meta:set_string("infotext", "Locked " .. furniture_description .. "\nOwned by " .. playername .. ".")
+            end,
+            on_rightclick = function(pos, node, clicker)
+                local meta = minetest.get_meta(pos)
+                local owner = meta:get_string("owner")
+                local playername = clicker:get_player_name()
+                if (playername == owner) then
+                    minetest.swap_node(pos, {name = furniture_name .. "_locked_activated", param2 = node.param2})
+                    minetest.sound_play(fdef.activate_sound[sname] or fdef.activate_sound.default,
+                    {pos = pos, max_hear_distance = 10}, true)
                 else
-                    special_node = furniture.types[i].special_material
-                    special_definition = minetest.registered_nodes[special_node]
-                    special_sname = string.match(special_node, ':(.*)')
-                    tiles = {"variations_" .. sname .. ".png", special_definition.tiles[1]}
-                    alpha = special_definition.use_texture_alpha
-                    furniture_name = furniture_name .. "_" .. special_sname
+                    minetest.chat_send_player(playername, "This Locked " .. furniture_description .. " is protected by " .. owner .. ".")
                 end
             end
-            local collision_box = {
-                type = "fixed",
-                fixed = furniture.types[i].box,
-            }
-            minetest.register_node(furniture_name, {
-                description = furniture_description,
-                tiles = tiles,
-                groups = base_definition.groups,
-                drawtype = 'mesh',
-                mesh = furniture_mesh,
-                collision_box = collision_box,
-                selection_box = collision_box,
-                use_texture_alpha = alpha,
-                paramtype = "light",
-                paramtype2 = "facedir",
-                sunlight_propagates = true, -- Its a mesh so I am assuming it has a few holes in it
-            })
-            --[[
-            if (furniture.types[i].storage) then
-                crates.register_storage(furniture_name, {
-                    description = furniture_description,
-                    tiles = tiles,
-                    groups = base_definition.groups,
-                    drawtype = 'mesh',
-                    mesh = furniture_mesh,
-                    collision_box = collision_box,
-                    selection_box = collision_box,
-                    use_texture_alpha = alpha,
-                    storage = furniture.types[i].storage * 8,
-                    paramtype = "light",
-                    paramtype2 = "facedir",
-                    sunlight_propagates = true, -- Its a mesh so I am assuming it has a few holes in it
-                })
-            else
-                minetest.register_node(furniture_name, {
-                    description = furniture_description,
-                    tiles = tiles,
-                    groups = base_definition.groups,
-                    drawtype = 'mesh',
-                    mesh = furniture_mesh,
-                    collision_box = collision_box,
-                    selection_box = collision_box,
-                    use_texture_alpha = alpha,
-                    paramtype = "light",
-                    paramtype2 = "facedir",
-                    sunlight_propagates = true, -- Its a mesh so I am assuming it has a few holes in it
-                })
-            end]]--
-        end
+        })
+        minetest.register_node(furniture_name .. "_locked_activated", {
+            description = furniture_description,
+            tiles = tiles,
+            groups = groups_hidden,
+            drawtype = 'mesh',
+            mesh = (fdef.base or fdef.name) .. "_activated.obj",
+            collision_box = collision_box_activated,
+            selection_box = collision_box_activated,
+            use_texture_alpha = alpha,
+            paramtype = "light",
+            paramtype2 = "facedir",
+            sunlight_propagates = true,
+            sounds = sounds,
+            drop = furniture_name .. "_locked",
+            on_rightclick = function(pos, node, clicker)
+                local meta = minetest.get_meta(pos)
+                local owner = meta:get_string("owner")
+                local playername = clicker:get_player_name()
+                if (playername == owner) then
+                    minetest.swap_node(pos, {name = furniture_name .. "_locked", param2 = node.param2})
+                    minetest.sound_play(fdef.deactivate_sound[sname] or fdef.deactivate_sound.default,
+                    {pos = pos, max_hear_distance = 10}, true)
+                else
+                    minetest.chat_send_player(playername, "This Locked " .. furniture_description .. " is protected by " .. owner .. ".")
+                end
+            end
+        })
     end
 end
 

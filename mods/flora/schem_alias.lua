9870e253e9f4393ccf2b0ec8f77fc3f9b8bb1d96
neinwhal <3191240+neinwhal@users.noreply.github.com>
2023-04-11 13:51:05 +0800

    fix schematic alias

---
+++ mods/flora/schem_alias.lua
@@ -0,0 +1,34 @@
+-- provide alias for the various default flora schematics
+minetest.register_alias("default:leaves", "flora:leaves")
+minetest.register_alias("default:apple", "flora:apple")
+minetest.register_alias("default:apple_mark", "flora:apple_mark")
+minetest.register_alias("default:tree", "flora:tree")
+
+minetest.register_alias("flowers:mushroom_brown", "flora:mushroom_brown")
+minetest.register_alias("flowers:mushroom_red", "flora:mushroom_red")
+
+minetest.register_alias("default:jungletree", "flora:jungletree")
+minetest.register_alias("default:jungleleaves", "flora:jungleleaves")
+minetest.register_alias("default:pine_tree", "flora:pine_tree")
+minetest.register_alias("default:pine_needles", "flora:pine_needles")
+minetest.register_alias("default:acacia_tree", "flora:acacia_tree")
+minetest.register_alias("default:acacia_leaves", "flora:acacia_leaves")
+minetest.register_alias("default:aspen_tree", "flora:aspen_tree")
+minetest.register_alias("default:aspen_leaves", "flora:aspen_leaves")
+
+minetest.register_alias("default:cactus", "flora:cactus")
+minetest.register_alias("default:papyrus", "flora:papyrus")
+minetest.register_alias("default:dry_shrub", "flora:dry_shrub")
+minetest.register_alias("default:junglegrass", "flora:junglegrass_1")
+
+minetest.register_alias("default:dirt", "blocks:dirt")
+minetest.register_alias("default:dry_dirt", "blocks:dry_dirt")
+
+minetest.register_alias("default:bush_stem", "flora:bush_stem")
+minetest.register_alias("default:bush_leaves", "flora:bush_leaves")
+minetest.register_alias("default:blueberry_bush_leaves_with_berries", "flora:blueberry_bush_leaves_with_berries")
+minetest.register_alias("default:blueberry_bush_leaves", "flora:blueberry_bush_leaves")
+minetest.register_alias("default:acacia_bush_stem", "flora:acacia_bush_stem")
+minetest.register_alias("default:acacia_bush_leaves", "flora:acacia_bush_leaves")
+minetest.register_alias("default:pine_bush_stem", "flora:pine_bush_stem")
+minetest.register_alias("default:pine_bush_needles", "flora:pine_bush_needles")

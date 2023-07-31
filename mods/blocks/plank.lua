16841ff54472393bc3f42dea5e6a96ceecde2410
theraven262 <corvus262@gmail.com>
2023-07-31 15:32:11 +0200

    Add viridis and azure plank variations, update other mushroom variations

--- mods/blocks/plank.lua
+++ mods/blocks/plank.lua
@@ -133,6 +133,16 @@ minetest.register_node("blocks:ghost_wood", {
 	sounds = default.node_sound_wood_defaults(),
 })
 
+minetest.register_node("blocks:viridis_wood", {
+	description = S("Viridis Mushroom Planks"),
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	tiles = {"blocks_viridis_wood.png"},
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2, wood = 1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
 minetest.register_craftitem("blocks:firebranches", {
 	description = ccore.comment("Pile of Glowing Sticks", "An excellent fuel source"),
 	inventory_image = "blocks_firebranches.png",

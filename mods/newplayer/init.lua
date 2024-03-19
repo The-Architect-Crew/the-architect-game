174137abb4d8f8ff70668cea541e1f4210b44ac7
theraven262 <corvus262@gmail.com>
2024-03-19 22:13:04 +0100

    Implement initial item giving

---
+++ mods/newplayer/init.lua
@@ -0,0 +1,11 @@
+minetest.register_on_joinplayer(function(ObjectRef, last_login)
+    if last_login == nil then
+        local inv = ObjectRef:get_inventory()
+        inv:add_item("main", "tools:pick_wood")
+        inv:add_item("main", "tools:axe_wood")
+        inv:add_item("main", "tools:shovel_wood")
+        inv:add_item("main", "protection:marker 4")
+        inv:add_item("main", "protection:protector")
+        inv:add_item("main", "blocks:rope 99")
+    end
+end)

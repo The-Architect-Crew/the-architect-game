b100cea80b6e8b8f0f9a57edc803e54f6ef9a587
theraven262 <corvus262@gmail.com>
2024-03-20 19:33:29 +0100

    Fix furnace crafting

--- mods/furniture/furnace.lua
+++ mods/furniture/furnace.lua
@@ -1,15 +1,16 @@
 furniture.furnace_materials = {
-	"blocks:stone",
-	"blocks:desert_stone",
-	"blocks:sandstone",
-	"blocks:obsidian",
-    "blocks:basalt",
-	"blocks:steelblock",
-	"blocks:rustblock"
+	{"blocks:stone", "blocks:cobble"},
+	{"blocks:desert_stone", "blocks:desert_cobble"},
+	{"blocks:sandstone", "blocks:sandstone"},
+	{"blocks:obsidian", "blocks:obsidian"},
+    {"blocks:basalt", "blocks:basalt_cobble"},
+	{"blocks:steelblock", "blocks:steel_ingot"},
+	{"blocks:rustblock", "blocks:rust_ingot"}
 }
 
 for i=1,#furniture.furnace_materials do
-    local material = furniture.furnace_materials[i]
+    local material = furniture.furnace_materials[i][1]
+    local crafting_material = furniture.furnace_materials[i][2]
 	local sname = string.match(material, ':(.*)')
 
     workbench.register_furnace("furniture", material)
@@ -17,9 +18,9 @@ for i=1,#furniture.furnace_materials do
     minetest.register_craft({
         output = "furniture:furnace_" .. sname,
         recipe = {
-            {material, material, material},
-            {material, "", material},
-            {material, material, material},
+            {crafting_material, crafting_material, crafting_material},
+            {crafting_material, "", crafting_material},
+            {crafting_material, crafting_material, crafting_material},
         }
     })
 end
